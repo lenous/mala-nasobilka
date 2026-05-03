12a9633620d9a327e9d1d6e61127e2961e6f26e6
Lenny <58422978+lenous@users.noreply.github.com>
2026-05-03 16:40:54 +0200

    Bump cache after factor range fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mala-nasobilka-v6'
+const CACHE_NAME = 'mala-nasobilka-v7'
 const BASE = '/mala-nasobilka/'
 const ASSETS = [BASE, `${BASE}manifest.webmanifest`, `${BASE}app-icon.svg`]
 
