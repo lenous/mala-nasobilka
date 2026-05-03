d4c364755848578f0abce0d40ebb3085ae9b99fd
Lenny <58422978+lenous@users.noreply.github.com>
2026-05-03 16:51:54 +0200

    Bump cache after right operand test

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mala-nasobilka-v8'
+const CACHE_NAME = 'mala-nasobilka-v9'
 const BASE = '/mala-nasobilka/'
 const ASSETS = [BASE, `${BASE}manifest.webmanifest`, `${BASE}app-icon.svg`]
 
